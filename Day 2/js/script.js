d4ead477607404a98c5d15864d69a1e7f0b52905
Rendyscarlet123 <rendy9622@gmail.com>
2019-11-29 20:32:57 +0700

    Day's 3

--- Day 2/js/script.js
+++ Day 2/js/script.js
@@ -63,8 +63,74 @@ let add = function(x,y,z){
     return x + z +y
 }
 
-let getScore = function ( nama = 'jhon',scores = 80){
-    return `${nama} Score: ${scores}`
+let umurs = function(){
+    return 25;
 }
-console.log(getScore('Rendy',100));
+let getScore = function ( nama = 'jhon',scores = 80){
+    return nama + " " + "Umur : " + umurs();
+}
+
+let myBook = {
+    title : "How I Meet your Mother",
+    author : "Jhon F. Doe",
+    pageCount : 326
+}
+
+let getBook  = function(book){
+    console.log(book.title)
+}
+
+let myAccount = {
+    name: 'jhon Doe',
+    expense: 0,
+    income: 0
+}
+
+let addExpense = function(account,expense){
+    console.log();
+}
+
+//string methods
+
+let text = "Jhon"
+console.log(text.length);
+console.log(text.toUpperCase());
+console.log(text.toLowerCase());
+
+let password = "random123"
+console.log(password.includes('random123'));
+
+// number method
+
+let num = 514.123;
+
+console.log(num.toFixed(2));
+console.log(Math.round(num));
+console.log(Math.floor(num));
+console.log(Math.ceil(num));
+
+//array
+
+let emptyArray = [];
+let array = [1,2,3,5,6,7];
+console.log(array[1]);
+
+let nameList = [
+    'Rendy',
+    'Praseptya',
+    'Nugraha'
+];
+
+nameList.push('Dani');
+nameList.unshift('Pedrosa');
+// nameList.pop();
+// nameList.shift();
+nameList[0] = "Rossi";
+
+let activities = ['Tidur','Ngoding','PUBG'];
+activities.forEach(function(value,index){
+    console.log(index);
+    console.log(value);
+});
+console.log(activities.indexOf('Tidur'));
 
---
+++ Day 3/js/script.js
@@ -0,0 +1,41 @@
+// DOM
+// Melakukan manipulasi dom HTML
+// Query
+// Query Selector
+// const element = document.querySelector('p');
+// // element.textContent = `${element.textContent} I'm From Bandung`
+// // create element 
+// // append child
+// // const newParagraph = document.createElement('p');
+// // newParagraph.textContent = 'Rendy';
+// // document.querySelector('.p1').appendChild(newParagraph);
+// // handling user interaction
+// const button = document.querySelector('button')
+// document.querySelector('button').
+// addEventListener('click',function(e){
+//     e.target.textContent = "Button clicked"
+// })
+
+// const input = document.querySelector('input');
+// document.querySelector('input').
+// addEventListener('input',function(e){
+//     element.textContent = e.target.value
+//     button.textContent = e.target.value
+//     console.log(e.target.value)
+// })
+
+// document.querySelector('#check').
+// addEventListener('change',function(e){
+//     console.log(e.target.checked)
+// })
+
+// document.querySelector('#select').
+// addEventListener('change',function(e){
+//     console.log(e.target.value)
+// })
+
+// Data Storage
+// Acces local storage 
+// set and get item
+
+window.localStorage.setItem('username','Bambang');
